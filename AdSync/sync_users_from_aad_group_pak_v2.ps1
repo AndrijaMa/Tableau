4251d58a407415396ddbbddad1dec0b130c10443
AndrijaMa <60138623+AndrijaMa@users.noreply.github.com>
2020-04-16 14:51:50 +0200

    Final

--- AdSync/sync_users_from_aad_group_pak_v2.ps1
+++ AdSync/sync_users_from_aad_group_pak_v2.ps1
@@ -7,7 +7,7 @@ $client_id = ""
 $client_secret = ""
 
 #The name of the Azure AD groups that the script will import the users from 
-$SecurityGroups = @("Creators","Explorer")
+$SecurityGroups = @("SE_Tableau")
 
 #Tableau Server information 
 #Enter the server base url
@@ -28,79 +28,143 @@ $siteRole = 'Viewer'
 ###################################################################################################
 ###########Dont change anything below this line####################################################
 ###################################################################################################
-
+#Define paths to the Graph API
 $ms_online_url = 'https://login.microsoftonline.com/'
 $auth_url = $ms_online_url+$tenant_Id+'/oauth2/v2.0/token'
 $ms_graph_url = 'https://graph.microsoft.com/'
 $scope = $ms_graph_url+'.default'
 
+#Tableau atthentication API
 $ts_auth_url = $ts_url+'/api/'+$ts_api_ver+'/auth/signin'
 
 #Get Tableau Acess Token
 $ts_headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $ts_headers.Add("Content-Type", "text/xml")
 
-$ts_body = "<tsRequest>`n	<credentials`n	  personalAccessTokenName=`"$personalAccessTokenName`" personalAccessTokenSecret=`"$personalAccessTokenSecret`" >`n  		<site contentUrl=`"`" />`n	</credentials>`n</tsRequest>"
+#Tableau request body
+$ts_body = "<tsRequest>`n	<credentials`n	  personalAccessTokenName=`"$personalAccessTokenName`" personalAccessTokenSecret=`"$personalAccessTokenSecret`" >`n  		<site contentUrl=`"$site_content_uri`" />`n	</credentials>`n</tsRequest>"
 $ts_token_response = Invoke-RestMethod $ts_auth_url -Method 'POST' -Headers $ts_headers -Body $ts_body -SkipCertificateCheck
 $ts_token_response = $ts_token_response.tsResponse
 
+#Get the token that is used for authentication
 $ts_token = $ts_token_response.credentials.token
+#Get site id
 $ts_site = $ts_token_response.credentials.site.id
+#The url to the tableau site
 $ts_site_url = $ts_url+"/api/"+$ts_api_ver+"/sites/"+$ts_site+"/users"
 
+#Tableau header
 $ts_user_headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $ts_user_headers.Add("Content-Type", "application/xml")
 $ts_user_headers.Add("x-tableau-auth", $ts_token)
 
-$ts_user_response = Invoke-RestMethod $ts_site_url -Method 'GET' -Headers $ts_user_headers -SkipCertificateCheck
-$ts_users = $ts_user_response.tsResponse.users.user.name
- 
+#Set the page for the loop
+$page = 1
+#Define the Array that will hold the Tableau users
+$ts_users = @()
+do{
+        #Define base url to support paging
+        $ts_site_url = $ts_url+"/api/"+$ts_api_ver+"/sites/"+$ts_site+"/users?pageSize=100&pageNumber="+$page
+        
+        $ts_user_response = Invoke-RestMethod $ts_site_url -Method 'GET' -Headers $ts_user_headers -SkipCertificateCheck     
+        #Get number totan number of objects 
+        [int]$totalAvailable = $ts_user_response.tsResponse.pagination.totalAvailable
+        #Get the size of the page. Defualt is 50
+        [int]$pageSize = $ts_user_response.tsResponse.pagination.pageSize
+        #Get the number of pages  and round it up 
+        $maxPages = [math]::ceiling([int]$totalAvailable / [int]$pageSize)
+    
+    
+    if($ts_user_response.tsResponse.users){
+             #Fetch the Tableau username from the site that you defined in site_content_uri
+             foreach($tsu in $ts_user_response.tsResponse.users)
+             {
+                $obj = New-Object -TypeName PSObject
+                $obj | Add-Member -MemberType NoteProperty -Name user -Value $tsu.user.name  
+                $ts_users += $obj  
+             }
+             
+         }
+        #Do a loop if there are more than one pages in the loop
+         if($maxPages -gt 1)
+        {
+                $page++
+        }        
+        
+}until([int]$page -eq [int]$maxPages)
+
 #Get Azure Access Token
 $az_headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $az_headers.Add("Content-Type", "application/x-www-form-urlencoded")
+
 #$headers.Add("SdkVersion", "postman-graph/v1.0")
 $az_body = "grant_type=client_credentials&client_id="+$client_id+"&client_secret="+$client_secret+"&scope="+$scope
 $az_token_response = Invoke-RestMethod $auth_url -Method 'POST' -Headers $az_headers -Body $az_body -SkipCertificateCheck
 
 #List Azure AD Users
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+
 #$headers.Add("SdkVersion", "postman-graph/v1.0")
 $headers.Add("Authorization", "Bearer "+ $az_token_response.access_token )
 
+#Create a array  to hold the Azure users
+$az_users = @()
+#Loop thru all Security groups defined in SecurityGroups
 foreach($SecurityGroup in $SecurityGroups)
 {
     $SecurityGroup = "'$SecurityGroup'"
+
     $group_url = $ms_graph_url+'v1.0/groups?$filter=displayName eq '+$SecurityGroup
+    
     $az_response = Invoke-RestMethod $group_url -Method 'GET' -Headers $headers -SkipCertificateCheck
     
     #| Select-Object displayName, userPrincipalName
     $az_aad_group = $az_response.value.id
+    $member_uri = $ms_graph_url+"v1.0/groups/$az_aad_group/members"
+   
+    do{
+        $az_aad_group_members = Invoke-RestMethod $member_uri -Method 'GET' -Headers $headers -SkipCertificateCheck
+         
+        foreach($azgm in $az_aad_group_members.value)
+        {
+            
+                 $obj1 = New-Object -TypeName PSObject
+                 $obj1 | Add-Member -MemberType NoteProperty -Name mail -Value $azgm.mail
+                 $obj1 | Add-Member -MemberType NoteProperty -Name displayName -Value $azgm.displayName
+                 $az_users += $obj1                    
+        }
+        $member_uri = $az_aad_group_members.'@odata.nextlink'
     
-    $az_aad_group_members = Invoke-RestMethod  $ms_graph_url"v1.0/groups/$az_aad_group/members" -Method 'GET' -Headers $headers -SkipCertificateCheck
-    $az_users += $az_aad_group_members.value.mail
+    }until (!($member_uri))
     
 }
-#Remove duplicates users
-$az_users = $az_users |  Select-Object -Unique
-
-$ts_site_id = $ts_url+"/api/"+$ts_api_ver+"/sites/"+$ts_site_name+"?key=name"
+#$az_users 
+#Call the site that you want to import the users to
+$ts_site_id = $ts_url+"/api/"+$ts_api_ver+"/sites/"+$site_content_uri+"?key=name"
 
 $siteid = Invoke-RestMethod $ts_site_id -Method 'GET' -Headers $ts_user_headers  -SkipCertificateCheck
 $siteid = $siteid.tsResponse.site.ID
 
-$delta = Compare-Object $ts_users $az_users | Where-Object{$_.SideIndicator -eq '=>'}
-
-#Loop over all users and add the AD users that are missing in Tableau that contain the filter value if a filter is in use 
-ForEach ($user in $delta.InputObject)
-{
-        try{
-            
-                $ts_user_body = "<tsRequest>`n	<user name=`"$user`" siteRole=`"$SiteRole`">`n		`n	</user>`n</tsRequest>"
-                $create = Invoke-RestMethod $ts_site_url -Method 'POST' -Headers $ts_user_headers -Body $ts_user_body -SkipCertificateCheck
+#Copare the list of Azure users and Tableau users and return Azure users that do not exist on the Tableau server
+$delta = $(Compare-Object $($ts_users.user)  $($az_users.mail)  | Where-Object{$_.SideIndicator -eq '=>'}).InputObject
+$delta 
+#Loop over all Azure users and filter to only return the users that match the delta
+ ForEach ($user in $az_users | Where-Object{$_.mail -in $delta})
+ {
+         try{
+                 #Add user to site
+                 $user_id = $response.tsResponse.user.id
+                 $ts_user_body = "<tsRequest>`n	<user name=`"$($user.mail)`" siteRole=`"$SiteRole`">`n		`n	</user>`n</tsRequest>"
+                 $create = Invoke-RestMethod $ts_site_url -Method 'POST' -Headers $ts_user_headers -Body $ts_user_body -SkipCertificateCheck
+                 $user_id = $create.tsResponse.user.id
                 
-                
-        }
-        catch{
-                $PSItem.Exception.Message
-        }
-}
+                 #Update user properties
+                 $ts_update_user_body = "<tsRequest>`n	<user	email=`"$($user.mail)`"`n			fullName=`"$($user.displayName)`"`n			/>`n</tsRequest>"#                         $ts_update_user_body 
+                 $ts_update_url = $ts_url+'/api/'+$ts_api_ver+'/sites/'+$siteid+'/users/'+$user_id
+                 $update = Invoke-RestMethod $ts_update_url -Method 'PUT' -Headers $ts_user_headers -Body $ts_update_user_body  -SkipCertificateCheck
+                        
+         }
+         catch{
+                 $PSItem.Exception.Message
+         }
+ }
---
+++ AzureRM-Deploy/deploy.ps1
@@ -0,0 +1,312 @@
+param   (
+    [Parameter(Mandatory=$true)]
+    [ValidateScript(
+        {
+            If ($_ -match '^[0-9A-Za-z]{4}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{4}$'){
+                $True
+            } 
+            elseif($_.ToLower() -eq 'trial') {
+                $True
+            }
+            Else 
+            {
+                Throw 'Aborting installation due to invalid license key'
+            }
+        }
+        )
+    ]$LicenseKey,
+    [Parameter(Mandatory=$false)] [String]$Bootstrap=$false,
+    [Parameter(Mandatory=$false)] [String]$Help,
+    [Parameter(Mandatory=$true,HelpMessage="Please enter the version of Tableau Server that you want to download (Example:2019.3.4")] 
+    [ValidateLength(8,9)]
+    [ValidateScript(
+                    {
+                        If ($_ -match '\d{4}[.]\d{1}[.]\d{1,2}') 
+                        {
+                            $True
+                        } 
+                        Else 
+                        {
+                            Throw "$_ does not match the expected format. The correct format should be xxxx.xx.xx (Example:2019.4.1)"
+                        }
+                    }
+                    )
+    ]
+    [string]$version=2019.3.4
+)
+function func_Version ($version) {
+   
+    if(!$Version)
+    {
+        Write-Host "-Version is missing a value. It should be in the format xxxx.x.x like for example 2019.1.4 or type Trial to active a 14 day trial"
+    }
+    elseif($version.ToString().Length -ne 8)
+    {
+        Write-Host "-Version is in the wrong format. It should be in the format xxxx.x.x like for example 2019.1.4"
+        
+    }
+
+    elseif($version.ToString().Length -eq 8)
+            {
+            if ($version -like '*.*')
+            {
+                $global:major = $version.substring(0,4)
+                $global:minor = $version.substring(0,$version.lastindexof('.')).substring(5)
+                $global:hotfix = $version.substring($version.length-1)
+                
+            }
+            elseif ($version -like '*-*')
+            {
+                $version = $version.ToString().replace('-','.') 
+                $global:major = $version.substring(0,4)
+                $global:minor = $version.substring(0,$version.lastindexof('.')).substring(5)
+                $global:hotfix = $version.substring($version.length-1)
+                
+            }
+        }
+        #return $global:major, $global:minor, $global:hotfix
+}
+
+$github_url = "https://raw.githubusercontent.com/AndrijaMa/TestX/master/"
+$folder = "C:\Downloads\"
+$reg_file = "reginfo.json"
+$iDP_config = "iDP_config.json"
+$log_file = "install.log"
+$event_file = "event.log"
+$bootstrapfile = "bootstrap.json"
+
+$global:major = ''
+$global:minor = ''
+$global:hotfix = ''
+$global:DownloadFile = ''
+
+
+function Write-ToLog ($text) {
+    
+    $message = "[{0:yyyy/MM/dd/} {0:HH:mm:ss}]" -f (Get-Date) +", "+ $text 
+    Write-Host  $message
+    Write-Output $message | Out-file $folder$event_file -Append -Encoding default
+
+}
+
+function func_Download($github_url, $folder, $reg_file, $iDP_config, $log_file, $event_file,$version_major, $version_minor, $version_hotfix){
+    
+    try{#Set the path  to the server version of Tableau that you want to download
+        $global:DownloadFile = "TableauServer-64bit-"+$version_major+"-"+$version_minor+"-"+$version_hotfix+".exe"
+        $url = "https://downloads.tableau.com/esdalt/"+$version_major+"."+$version_minor+"."+$version_hotfix+"/"+$DownloadFile
+
+        if(Test-Path $folder)
+        {
+            Write-ToLog -text  "'The ' $folder 'folder already exists'"
+        }
+        else
+            {
+                New-Item -Path $folder -ItemType Directory
+                Write-ToLog -text  'Created folder ' $folder
+            }
+        Write-ToLog -text $url
+        #Download the server installation file
+        if(Test-Path $($folder+$global:DownloadFile))
+        {
+            Write-ToLog -text "Downloading Tableau Server installation media download..." 
+            Write-ToLog -text  $($folder+$DownloadFile) ' exists'
+        }
+        else
+        {
+            
+            Invoke-WebRequest -Uri $url -OutFile $($folder+$DownloadFile)
+            Write-ToLog -text "Download of Tableau Server installation media completed successfully"    
+            Write-ToLog -text "The download is" (Get-Item $($folder+$DownloadFile)).length/1GB " GB and the download took " 
+        }
+        
+
+        #Download reg_file
+        Write-ToLog -text  "Downloading regfile"
+        if(Test-Path $($folder+$reg_file))
+        {
+            Write-ToLog -text  $($folder+$reg_file) ' already exists'
+        }
+        else
+        {
+            Invoke-WebRequest -Uri $github_url$reg_file -OutFile $folder$reg_file
+            Write-ToLog -text "Download of regfile completed successfully"
+        }
+
+        #Download iDP file
+        Write-ToLog -text  "Downloading iDP config file"
+        if(Test-Path $($folder+$iDP_config))
+        {
+            Write-ToLog -text  $($folder+$iDP_config) ' already exists'
+        }
+        else {
+            Invoke-WebRequest -Uri $github_url$iDP_config -OutFile $folder$iDP_config
+            Write-ToLog -text "Download of iDP config file completed successfully"            
+        }
+
+    }
+    catch
+        {
+            Write-ToLog -text $PSItem.Exception.Message
+        }
+}
+
+function func_Install($file_path, $log_path)
+{
+    
+        try {
+
+                #Install Switches and Properties for Tableau Server
+                #https://help.tableau.com/current/server/en-us/silent_installer_flags.htm
+                #Start silent Tableau server installation
+
+                if((Test-Path HKLM:\SOFTWARE\Tableau\) -eq $false)
+                {
+                    Write-ToLog -text  "Starting Tableau Server installation"
+                    if($global:major -le 2019 -and $global:minor -lt 4 -or $global:major -le 2018 ){
+                        Start-Process -FilePath $file_path -ArgumentList " /install /silent /ACCEPTEULA = 1 /LOG '$log_path'" -Verb RunAs -Wait
+                    }
+                    elseif ($global:major -ge 2019 -and $global:minor -eq 4 -or $global:major -ge 2020) {
+                        Start-Process -FilePath $file_path -ArgumentList " /install /passive ACCEPTEULA=1" -Verb RunAs -Wait
+                    }
+
+                    
+                    Write-ToLog -text  "Tableau Server installation completed successfully"
+                }
+                else
+                {
+                    Write-ToLog -text  'Tableau server is already installed'
+                }
+
+                #Identifying path to TSM
+                #Get-ItemProperty -Path HKLM:\SOFTWARE\Tableau $version_full*
+                Write-ToLog -text "Adding TSM to local Windows system PATH"
+    
+                #Check if Tableau is installed on the Server
+                if((Test-Path HKLM:\SOFTWARE\Tableau\) -eq $true)
+                {
+                    #Get the AppVersion Property from the registry that contains the path to the 
+                    if ( (Get-Item "HKLM:\SOFTWARE\Tableau\Tableau Server *\Directories" | Get-ItemProperty | Select-Object Application).Application -eq '\$')
+                    {
+                        $packages =  ((Get-Item "HKLM:\SOFTWARE\Tableau\Tableau Server *\Directories" | Get-ItemProperty | Select-Object Application).Application+"Packages")
+                    }
+                    else
+                    {
+                        $packages =  ((Get-Item "HKLM:\SOFTWARE\Tableau\Tableau Server *\Directories" | Get-ItemProperty | Select-Object Application).Application+"\Packages")
+                    }
+                    
+                    
+                    $bin = (Get-ItemProperty ($packages+"\bin.*") | Select-Object Name).Name
+                    $global:tsm_path = $packages+"\"+$bin+"\";
+                    #Add TSM to Windows Path
+
+                    $Env:path += $global:tsm_path
+
+
+                }
+
+                #Generate bootstrap file
+                if($Bootstrap -eq $true)
+                {
+                    Write-ToLog -text  "Creating bootstrap file in " $folder
+                    Invoke-Expression "tsm topology nodes get-bootstrap-file --file '$($folder+$bootstrapfile)'"
+                }
+        }
+        catch
+            {
+                Write-ToLog -text $PSItem.Exception.Message
+            }
+}
+
+function func_AntiVirus(){
+        #Disable antivirus scan for the folder that is being used during the installation
+        Write-ToLog -text "Adding C:\Downloads to AV Exlusion"
+        Add-MpPreference -ExclusionPath $folder
+
+        if((Test-Path HKLM:\SOFTWARE\Tableau\) -eq $true){
+            $ts_install = (Get-Item "HKLM:\SOFTWARE\Tableau\Tableau Server *\Directories" | Get-ItemProperty | Select-Object Application).Application
+            Write-ToLog -text $ts_install
+
+            $ts_data =  (Get-Item "HKLM:\SOFTWARE\Tableau\Tableau Server *\Directories" | Get-ItemProperty | Select-Object Data).Data   
+            Write-ToLog -text $ts_data 
+            
+            Add-MpPreference -ExclusionPath $ts_install 
+            Write-ToLog -text "Added Tableau server install folder to AntiVirus Exlusions"
+            Add-MpPreference -ExclusionPath $ts_data 
+            Write-ToLog -text "Added Tableau server data folder to AntiVirus Exlusions"
+        }
+        }   
+function func_Configure($folder, $reg_file, $iDP_config, $log_file, $event_file, $LicenseKey)
+{
+                                
+            try{
+                
+                $tsm = $global:tsm_path +"tsm.cmd"
+                Write-ToLog $tsm
+                #Activate Tableau Server license
+                Write-ToLog -text  "Registering Tableau Server License"
+
+                if($LicenseKey.ToLower() -eq 'trial'){
+                    
+                    Start-Process $tsm -ArgumentList " licenses activate -t" -Wait
+                    Write-ToLog -text "Completed Tableau Server trial license activation"
+                }
+                elseif($LicenseKey -match '^[0-9A-Za-z]{4}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{4}$'){
+                    
+                    Start-Process $tsm -ArgumentList " licenses activate -k $LicenseKey" -Wait
+                    Write-ToLog -text "Completed Tableau Server license activation"
+                }
+               
+                
+                #Register Tableau Server
+                $reg_file = $($folder+$reg_file)
+                Write-ToLog -text "Starting Tableau Server registration"
+                Start-Process $tsm -ArgumentList " register --file $reg_file" -Wait
+                #Invoke-Expression "tsm register --file '$reg_file'"
+                Write-ToLog -text "Completed Tableau Server registration"
+
+                #Set local repository
+                $iDP_file = $($folder+$iDP_config)
+                Write-ToLog -text "Starting Tableau Server local Repository setup"
+                Start-Process $tsm -ArgumentList " settings import -f $iDP_file" -Wait
+                #Invoke-Expression "tsm settings import -f '$iDP_file'"
+                Write-ToLog -text "Completed Tableau Server local Repository setup"
+
+                #Apply pending changes
+                Write-ToLog -text "Applying pending TSM changes"
+                Start-Process $tsm -ArgumentList " pending-changes apply" -Wait
+                #Invoke-Expression "tsm pending-changes apply"
+                Write-ToLog -text "TSM changes applied successfully."
+
+                #Initialize configuration
+                Write-ToLog -text "Initializing Tableau Server"
+                Start-Process $tsm -ArgumentList " initialize" -Wait
+                #Invoke-Expression "tsm initialize"
+                Write-ToLog -text "Tableau Server initialized"
+
+                #Initialize configuration
+                Write-ToLog -text "Starting Tableau Server"
+                Start-Process $tsm -ArgumentList " start"  -Wait
+                #Invoke-Expression "tsm start"
+                Write-ToLog -text "Tableau Server started"
+            }
+            catch
+            {
+                Write-ToLog -text $PSItem.Exception.Message
+            }
+}
+
+function func_main(){
+    #Exclude folders from realtime scanning
+    func_AntiVirus
+    #Set paramaters for the Tableau Server version
+    func_Version -Version $Version
+    #Download Tableau sewrver installation files
+    func_Download -github_url $github_url -folder $folder -reg_file $reg_file -iDP_config $iDP_config -log_file $log_file  -event_file $event_file -version_major $global:major -version_minor $global:minor -version_hotfix $global:hotfix
+    #Install Tableau server
+    func_Install -log_path $($folder+$log_file) -file_path $($folder+$global:DownloadFile)
+    #Configure tableau server
+    func_Configure -folder $folder -reg_file $reg_file -iDP_config $iDP_config -log_file $log_file  -event_file $event_file -LicenseKey $LicenseKey
+    func_AntiVirus
+}
+
+func_main
