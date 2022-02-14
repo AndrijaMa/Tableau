de1cc3212c74e62a28b4ab385e14289dab4320c8
AndrijaMa <60138623+AndrijaMa@users.noreply.github.com>
2022-02-14 17:25:56 +0100

    Update Inventory.py

--- Licensing/Inventory.py
+++ Licensing/Inventory.py
@@ -1,41 +1,94 @@
+####
+# This script contains functions that demonstrate how to move
+# collect user information from Tableau Server.
+#
+# To run the script, you must have installed Python 3.x or later, 
+#You will have to have the  'requests',bs4, pandas and math library installed:
+#
+# To run the script you need to modify the Tableau Portal url and Personal Access Token Name and Key,
+# where the server address has no trailing slash (e.g. http://localhost).
+#The script will only work on Tableau server version 2020.1 and newer
+
 from bs4 import BeautifulSoup  as bs
 import requests
 import pandas as pd
 import math
 
 base_url = '' #Tableau server base url with no trailing slash
-api_version = '' #Tableau Server API version (https://help.tableau.com/current/api/rest_api/en-us/REST/rest_api_concepts_versions.htm)
 name = "" #Personal Access Token Name (https://help.tableau.com/current/server/en-us/security_personal_access_tokens.htm)
 token = "" #Personal Access Token key
+bs_parser = 'html.parser'
+
 #****************************************************************************************************************
+#*****Do not change anything below this line
+#****************************************************************************************************************
+#Function used in the script*************************************************************************************
+api_version = '3.1'
+#****************************************************************************************************************
+def apicall(Method,url,payload,headers,):
+  result = bs((requests.request(Method, url,data=payload, headers=headers)).text, bs_parser)
+  return result
+#****************************************************************************************************************  
+def f_heades(token):
+  headers = {'x-tableau-auth': token, 'Content-Type': 'application/xml'}
+  return headers
+#****************************************************************************************************************
+def page_count(list):
+  count = int(list.pagination['totalavailable'])
+  page_count = math.ceil(int(list.pagination['totalavailable'])/100)
+  pageCount = 1 if count <= 100 else page_count
+  return pageCount
 #****************************************************************************************************************
 df_user = pd.DataFrame(columns=['SiteName''SiteID','UseName','UserRole'])
+#****************************************************************************************************************
 
 #Authenticate and save auth token
+#Tableau authentication url
 auth_url = base_url+"/api/"+api_version+"/auth/signin"
+#Define body
 payload = '<tsRequest>\n\t<credentials\n\t  personalAccessTokenName=\"'+name+'\" personalAccessTokenSecret=\"'+token+'\" >\n  \t\t<site contentUrl=\"\" />\n\t</credentials>\n</tsRequest>'
+#Define header
 headers = {'Content-Type': 'text/xml'}
-token = (bs((requests.request("POST", auth_url, headers=headers, data=payload)).text)).credentials['token']
+#Authentication token
+token = (apicall('POST', auth_url, payload, headers)).credentials['token']
+
+
+#Empty body
+payload=""
+#Collect Server API version
+#Server info url
+ver_url = base_url+"/api/"+api_version+"/serverinfo"
+#Authentication header
+headers = f_heades(token)
+api_version =  apicall('GET', ver_url, payload, headers).serverinfo.restapiversion.text
+
+#New base url with correct API version that was collected in the previous step
+base_url = base_url+"/api/"+api_version
 
 #List all sites and write them to a dataframe
-url = base_url+"/api/"+api_version+"/sites/"
+#Sites URL
+site_url = base_url+"/sites/"
 
 #Swithch between sites
-switchUrl = base_url+"/api/"+api_version+"/auth/switchSite"
+switchUrl = base_url+"/auth/switchSite"
 
-headers = {'x-tableau-auth': token,'Content-Type': 'application/xml'}
-sites =  bs((requests.request("GET", url, headers=headers)).text)
+headers = f_heades(token)
+#Call apicall function to collect a list of all sites
+sites =  apicall('GET', site_url, payload, headers)
 
-#Check how many loops will be executed
-sites_count = int(sites.pagination['totalavailable'])
-site_page_count = math.ceil(int(sites.pagination['totalavailable'])/100)
-sitePageCount = 1 if sites_count <= 100 else site_page_count
+#Check how many sites exist. If more than 100 sites are found then envoke paging and divide the number of sites by 100 to 
+#
+site_pageCount = page_count(sites)
 
+#Process site that were collected in the previous step
 x=1
-while x <= sitePageCount:
-  url_sites = base_url+"/api/"+api_version+"/sites?pageSize=100&pageNumber="+str(x)
+
+#while x <= sitePageCount:
+while x <= site_pageCount:
+  #print(x)
+  url_sites = base_url+"/sites?pageSize=100&pageNumber="+str(x)
   x = x +1
-  sites = bs(requests.request("GET", url_sites, headers=headers).text)
+  sites = apicall('GET', url_sites, payload, headers)
 
   for site in sites.sites:
 
@@ -43,37 +96,44 @@ while x <= sitePageCount:
     contentUrl = site['contenturl']
     siteName = site['name']
     siteID = site['id'] 
-    print("Processing site "+ siteName)
     
-    headers = {'x-tableau-auth': token, 'Content-Type': 'application/xml'}
+    headers = f_heades(token)
+    #print(headersx)
     payload = "<tsRequest>\t\n  \t\t<site contentUrl=\""+contentUrl+"\" />\n</tsRequest>"
-    token = (bs(requests.request("POST", switchUrl, data=payload,headers=headers).text)).credentials['token']
-
-    payload=""
-    headers = {'x-tableau-auth': token, 'Content-Type': 'application/xml'}
-    url_users = base_url+"/api/"+api_version+"/sites/"+siteID+"/users"
-    users = bs(requests.request("GET", url_users, headers=headers,data=payload).text)
+    #print("Payload: "+payloadx)
+    token = (apicall('POST', switchUrl, payload, headers)).credentials['token']
     
+    payload=""
+    headers = f_heades(token)
+    url_users = base_url+"/sites/"+siteID+"/users"
+    #print(final_url)
+    users = apicall('GET', url_users, payload, headers)
+    
+    #print(users)
     #Check how many loops will be executed
     user_count = int(users.pagination['totalavailable'])
-    user_page_count = math.ceil(int(users.pagination['totalavailable'])/100)
-    userPageCount = 1 if user_count <= 100 else user_page_count
-    
+
+    userPageCount = page_count(users)
+    print("Processing site "+ siteName +" found " + str(user_count) + " users.")
+
     uc=1
-    while uc <= user_page_count:  
-      users_url = base_url+"/api/"+api_version+"/sites/"+siteID+"/users?pageSize=100&pageNumber="+str(uc)
+    while uc <= userPageCount:  
+      #print("TotalPageCount: "+ str(userPageCount) +  " Current page: "+str(uc))
+      users_url = base_url+"/sites/"+siteID+"/users?pageSize=100&pageNumber="+str(uc)
       uc = uc + 1
       
-      userdata = bs(requests.request("GET", users_url, headers=headers,data=payload).text)
+      #print(users_url)
+      userdata = apicall('GET', url_users, payload, headers)
       
       for user in userdata.users:
+
         df_user = df_user.append({
                       'SiteID' : site['id'],
                       'SiteName' : site['contenturl'],
                       'UserName' : user['name'],
                       'UserRole' : user['siterole']
                     }, ignore_index=True,)
-        
+    
 df_user['UserRole'] = df_user['UserRole'].replace(['ServerAdministrator'],'Creator')
 df_user['UserRole'] = df_user['UserRole'].replace(['SiteAdministratorCreator'],'Creator')
 df_user['UserRole'] = df_user['UserRole'].replace(['ExplorerCanPublish'],'Explorer')
